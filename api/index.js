211af94ceb388fdb22bfdcc2e2350489e36257ee
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-10-06 13:19:26 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
 
+mongoose.connect(`${process.env.MONGO}`)
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
 
-app.listen(3000, () => console.log('Server running on port 3000 !!'));
+app.listen(3000, () => console.log(`Server running on port 3000 !! `));
