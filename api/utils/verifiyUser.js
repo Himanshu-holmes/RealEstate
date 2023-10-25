c1e446cb0b2db92b6abd1cd217a2418b1a22bcfd
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-10-25 10:16:42 +0530

    added sign out functionality

--- api/utils/verifiyUser.js
+++ api/utils/verifiyUser.js
@@ -3,7 +3,7 @@ import { errorHandler } from "./error.js";
 
 export const verifyToken = (req,res,next) => {
      const token = req.cookies.access_token;
-     console.log(token)
+     
 
      if(!token) return next(errorHandler(401,'unauthorized'));
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
   deleteUserSuccess,
   deleteUserFailure,
   deleteUserStart,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 
 import { useDispatch } from "react-redux";
@@ -111,6 +112,21 @@ export default function Profile() {
           dispatch(deleteUserFailure(error.message))
         }
   }
+const handleSignOut = async() => {
+  try {
+    dispatch(signOutUserStart());
+    const res = await fetch('api/auth/signout');
+    const data = await res.json();
+    if(data.success === false){
+      dispatch(deleteUserFailure(data.message));
+     return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -175,7 +191,7 @@ export default function Profile() {
 
       <div className="flex justify-between mt-5">
         <span className=" text-red-700 cursor-pointer " onClick={handleDeleteUser}>Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span className="text-red-700 cursor-pointer" onClick={handleSignOut}>Sign Out</span>
       </div>
       <p className=" text-red-700 mt-5">{error ? error : ''}</p>
       <p className=" text-green-700 mt-5">{updateSuccess ? 'User is updated successfully!' : ''}</p>
