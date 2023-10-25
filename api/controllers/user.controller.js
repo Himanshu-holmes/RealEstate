b115d89c550367f8400144262951202b11590c41
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-10-25 09:35:51 +0530

    added delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,4 +31,15 @@ export const updateUser = async(req,res, next) => {
     } catch (error) {
         next(error)
     }
+};
+
+export const deleteUser = async(req,res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'));
+
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json('User has been deleted!').clearCookie('access_token')
+    } catch (error) {
+        
+    }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifiyUser.js';
 
 
 const router = express.Router();
 
 router.get("/test",test);
-router.post('/update/:id', verifyToken, updateUser)
+router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
 
 export default router;
 
