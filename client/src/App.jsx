3cb88c9c87c5eb9d735c8710cb94a997c2d33847
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-10-31 10:23:06 +0530

    Created Search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/search" element={<Search/>}></Route>
         <Route path="/listing/:listingId" element={<Listing/>}/>
         <Route element={<PrivateRoute/>}> 
         <Route path="/profile" element={<Profile />} />
