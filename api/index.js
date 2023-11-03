69c5ebf993a36ca6c3209cee5deffa4bb79be5d5
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-11-03 17:40:33 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from "./routes/auth.route.js"
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path'
 
 
 const app = express();
@@ -14,11 +15,18 @@ mongoose.connect(`${process.env.MONGO}`)
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
 
+    const __dirname = path.resolve();
+
 app.listen(3000, () => console.log(`Server running on port 3000 !! `));
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
